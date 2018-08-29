45850e8ed5dab391ea60e03930b576610742419b
Stanley <stanley830912@gmail.com>
2018-08-28 22:14:54 -0400

    add dotfile script

---
+++ Documents/Script/dotfile/config.sh
@@ -0,0 +1,12 @@
+bare_repository_elements=("branches"  "config"  "description"  "HEAD"  "hooks"  "info"  "objects"  "refs")
+dotfiles=(".vimrc" ".bashrc" ".gitconfig" ".tmux.conf")
+github_repository_ssh_url="git@github.com:stanleyuan/dotfiles.git"
+github_repository_https_url="https://github.com/stanleyuan/dotfiles.git"
+bare_repository=".dotfiles"
+config_keyword="config"
+config_keyword=config
+config_keyword=config
+config_keyword=config
+config_keyword=config
+config_keyword=config
+config_keyword=config
