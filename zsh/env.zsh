d0b9523b0800e770c586fce6266739fe18998f0b
shihtiy-tw <shihtiy.tw@gmail.com>
2021-10-18 18:50:30 +0100

    [zsh]: export pythonpath

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -14,8 +14,8 @@ export PATH=${PATH}:${HOME}/.local/share/
 
 # Python
 #export PATH=${PATH}:${HOME}/Library/Python/3.9/bin
-PATHONPATH=$(which python)
-PYTHON3PATH=$(which ptyhon3)
+export PATHONPATH=$(which python)
+export PYTHON3PATH=$(which python3)
 
 # convenience
 if [ -d $HOME/Library/Python/3.9/bin ]; then
