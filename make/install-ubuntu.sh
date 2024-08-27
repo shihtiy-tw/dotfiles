f43547ff1a8a05d33740c3a9f7bec0b679e5886c
Shih-Ting Yuan <56125709+shihtiy-tw@users.noreply.github.com>
2024-08-27 21:58:15 +0100

    Update install-ubuntu.sh

--- make/install-ubuntu.sh
+++ make/install-ubuntu.sh
@@ -1,26 +1,22 @@
 # update and upgrade packages
-sudo apt-get update -y
+sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt-get install -y \
+sudo apt install -y \
     ninja-build \
     gettext libtool libtool-bin \
     autoconf automake cmake g++ \
     build-essential \
-    python3-dev \
+    python3-dev python3-pip\
     pkg-config unzip
 
-sudo apt-get install -y \
-    make build-essential libssl-dev zlib1g-dev \
+sudo apt install -y \
+    make libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-
-sudo apt-get -y install python3-pip -y
-
 # zsh
-sudo apt install zsh -y
-sudo apt-get install powerline fonts-powerline -y
+sudo apt install zsh powerline fonts-powerline -y
 
 # oh-my-zsh
 if [ ! -d ${HOME}/.oh-my-zsh ]; then \
@@ -44,6 +40,7 @@ fi
 if [ ! -d ${HOME}/.oh-my-zsh/custom/themes/spaceship-prompt ]; then \
   git clone https://github.com/denysdovhan/spaceship-prompt.git "${HOME}/.oh-my-zsh/custom/themes/spaceship-prompt"; \
   sed -i 's/^SPACESHIP_CHAR_SYMBOL=.*$/SPACESHIP_CHAR_SYMBOL="${SPACESHIP_CHAR_SYMBOL="$ "}"/' ${HOME}/.oh-my-zsh/custom/themes/spaceship-prompt/sections/char.zsh
+  git clone https://github.com/spaceship-prompt/spaceship-vi-mode.git ${HOME}/.oh-my-zsh/custom/plugins/spaceship-vi-mode
   sed -i 's/^SPACESHIP_VI_MODE_SHOW=.*$/SPACESHIP_VI_MODE_SHOW="${SPACESHIP_VI_MODE_SHOW=false}"/' ${HOME}/.oh-my-zsh/custom/themes/spaceship-prompt/sections/vi_mode.zsh
 fi
 
@@ -59,30 +56,16 @@ fi
 sudo apt install tmux -y
 
 # vim
-sudo apt-get install vim -y
+# sudo apt-get install vim -y
 
-# node
-#curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-#sudo apt-get install nodejs
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm i -g bash-language-server
-sudo npm install -g markdownlint-cli
+# fuse
+# to use appimage
+apt install libfuse2
 
 # neovim
-sudo apt-get install python3-neovim -y
-sudo npm install -g neovim
-wget -O /tmp/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x /tmp/nvim.appimage
-sudo rm -f /usr/bin/nvim
-#sudo ln -s ${HOME}/dotfiles/nvim.appimage /usr/bin/nvim
-sudo mv /tmp/nvim.appimage /usr/bin/nvim
-
-python3 -m pip uninstall pynvim neovim --user
-python3 -m pip install pynvim --user
-python3 -m pip install neovim --user
-
-pip install vim-vint
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage /usr/loca/bin/nvim
 
 # fzf
 if [ ! -d ${HOMW}/.fzf ]; then\
@@ -96,9 +79,10 @@ fi
 
 # auto jump
 if [ ! -d ./autojump ]; then \
-  git clone git://github.com/joelthelion/autojump.git /tmp/autojump; \
+  git clone https://github.com/wting/autojump.git /tmp/autojump; \
   cd /tmp/autojump
   python3 install.py; \
+  cd ${HOME}
 fi
 
 # python3
@@ -106,14 +90,7 @@ sudo ln -s $(which python3) /usr/local/bin/python
 
 [[ -s ${HOME}/.autojump/etc/profile.d/autojump.sh ]] && source ${HOME}/.autojump/etc/profile.d/autojump.sh
 
-# Go
-wget -q -O - https://git.io/vQhTU | bash
-
-# efm server
-go install github.com/mattn/efm-langserver@latest
-
 # Ag
-sudo apt-get install silversearcher-ag -y
-
+sudo apt install silversearcher-ag -y
 
 cd ${HOME}
