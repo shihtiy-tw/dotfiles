273fe3a01475d952bdc2229f56d8279bad3c0250
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-09-08 19:26:47 +0000

    [nvim]: setup moving key mapping

--- nvim/lua/config/keys.lua
+++ nvim/lua/config/keys.lua
@@ -14,3 +14,34 @@ require("nvim-tree").setup {}
 vim.api.nvim_set_keymap('n', '<F9>', '<cmd>NvimTreeFindFileToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('i', '<F9>', '<cmd>NvimTreeFindFileToggle<CR>', { noremap = true })
 
+-- move in panels
+vim.api.nvim_set_keymap('n', '<leader>h', '<c-w>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>j', '<c-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>k', '<c-w>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>l', '<c-w>l', { noremap = true })
+
+-- mini move
+  -- `HJKL` for moving visual selection (overrides H, L, J in Visual mode)
+  require('mini.move').setup({
+    mappings = {
+      left  = 'H',
+      right = 'L',
+      down  = 'J',
+      up    = 'K',
+    }
+  })
+
+  -- Shift + arrows
+  require('mini.move').setup({
+    mappings = {
+      left  = '<S-left>',
+      right = '<S-right>',
+      down  = '<S-down>',
+      up    = '<S-up>',
+
+      line_left  = '<S-left>',
+      line_right = '<S-right>',
+      line_down  = '<S-down>',
+      line_up    = '<S-up>',
+    }
+  })
