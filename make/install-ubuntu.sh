bf9a531868b2c8c7248bbe35a73872997a0c4b6e
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-09-08 13:08:57 +0000

    [ubuntu]: add riggrep install

--- make/install-ubuntu.sh
+++ make/install-ubuntu.sh
@@ -15,6 +15,8 @@ sudo apt install -y \
     libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+sudo apt-get install ripgrep
+
 # git-sim
 sudo apt install pipx
 sudo apt update
