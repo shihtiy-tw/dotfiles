2c16b06a771eb6813bb29ad3fafc2611aea5796d
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-09-09 23:27:52 +0000

    [nvim]: fix obsidian non-exisiting path

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -19,12 +19,27 @@ return {
   },
   opts = {
     workspaces = {
+      -- {
+      --   name = "brainiverse",
+      --   path = "~/Brainiverse/Brainiverse",
+      -- },
       {
-        name = "brainiverse",
-        path = "~/Brainiverse/Brainiverse",
+        name = "no-vault",
+        path = function()
+          -- alternatively use the CWD:
+          -- return assert(vim.fn.getcwd())
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        overrides = {
+          notes_subdir = vim.NIL,  -- have to use 'vim.NIL' instead of 'nil'
+          new_notes_location = "current_dir",
+          templates = {
+            folder = vim.NIL,
+          },
+          disable_frontmatter = true,
+        },
       },
     },
-
     -- see below for full list of options 👇
   },
 }
