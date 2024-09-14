99899fa4c7364dad4d3cdbde5680881bcb05c417
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-09-14 00:54:23 +0000

    [nvim]: shorten the url

--- nvim/lua/configs/render-markdown.lua
+++ nvim/lua/configs/render-markdown.lua
@@ -1,3 +1,25 @@
 require('render-markdown').setup({
-    file_types = { 'markdown', 'quatro' },
+  file_types = { 'markdown', 'quatro' },
+  link = {
+    -- Turn on / off inline link icon rendering
+    enabled = false,
+    -- Inlined with 'image' elements
+    image = '󰥶 ',
+    -- Inlined with 'email_autolink' elements
+    email = '󰀓 ',
+    -- Fallback icon for 'inline_link' elements
+    hyperlink = '󰌹 ',
+    -- Applies to the fallback inlined icon
+    highlight = 'RenderMarkdownLink',
+    -- Define custom destination patterns so icons can quickly inform you of what a link
+    -- contains. Applies to 'inline_link' and wikilink nodes.
+    -- Can specify as many additional values as you like following the 'web' pattern below
+    --   The key in this case 'web' is for healthcheck and to allow users to change its values
+    --   'pattern':   Matched against the destination text see :h lua-pattern
+    --   'icon':      Gets inlined before the link text
+    --   'highlight': Highlight for the 'icon'
+    custom = {
+      web = { pattern = '^http[s]?://', icon = '󰖟 ', highlight = 'RenderMarkdownLink' },
+    },
+  },
 })
