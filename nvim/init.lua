5ac7dc1005d042ee31372d1d8acbfbdb4d43ff2b
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-09-14 22:51:11 +0000

    [nvim]: fix lazy install

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 -- Auto-install lazy.nvim if not present
-if not vim.uv.fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
   print('Installing lazy.nvim....')
   vim.fn.system({
     'git',
