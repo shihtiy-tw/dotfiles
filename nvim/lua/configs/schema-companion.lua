6101653db64d4a0496a1d03f854479984ac22e81
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-09-15 21:04:12 +0000

    [nvim]: split schema-companion config

--- nvim/lua/configs/schema-companion.lua
+++ nvim/lua/configs/schema-companion.lua
@@ -1,21 +1,26 @@
 require("schema-companion").setup({
-  -- if you have telescope you can register the extension
+  -- log_level = vim.log.levels.DEBUG,
   log_level = vim.log.levels.DEBUG,
+  -- if you have telescope you can register the extension
   enable_telescope = true,
   formatting = true,
-  matchers = {
-    -- FIX: Need to debugy why I changed the matchers first and it will change the schemastore?
-    -- │ [schema-companion.nvim] [DEBUG] [...azy/schema-companion.nvim/lua/schema-companion/utils.lua:11]: Ensuring schema exists: uri="https://raw.githubusercontent.com/datreeio/CRDs-catalog/ma │
-    -- │ [schema-companion.nvim] [DEBUG] [...azy/schema-companion.nvim/lua/schema-companion/utils.lua:15]: Schema does not exist on remote: uri="https://raw.githubusercontent.com/datreeio/CRDs-c │
-    -- │ [schema-companion.nvim] [DEBUG] [...y/schema-companion.nvim/lua/schema-companion/context.lua:46]: bufnr=1 client_id=1 no registered matcher matched this file
-    --
-    -- add your matchers
-    -- require("schema-companion.matchers.kubernetes").setup({ version = "master" }),
-  },
-  schemas = {
-    {
-      name = "Kubernetes",
-      uri = require('kubernetes').yamlls_schema(),
-    },
-  }
+      matchers = {
+        -- add your matchers
+        -- require("schema-companion.matchers.kubernetes").setup({ version = "master" }),
+      },
+      schemas = {
+        {
+          name = "Kubernetes Main",
+          uri = require('kubernetes').yamlls_schema()
+        },
+        {
+          name = "Kubernetes v1.30",
+          uri =
+          "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.30.3-standalone-strict/all.json",
+        },
+        {
+          name = "Protolint",
+          uri = "~/.config/nvim/schemas/protolint.json",
+        },
+      },
 })
--- nvim/lua/plugins/schema-companion.lua
+++ nvim/lua/plugins/schema-companion.lua
@@ -8,30 +8,7 @@ return {
     { 'diogo464/kubernetes.nvim' }
   },
   config = function()
-    require("schema-companion").setup({
-      -- log_level = vim.log.levels.DEBUG,
-      -- if you have telescope you can register the extension
-      enable_telescope = true,
-      matchers = {
-        -- add your matchers
-        -- require("schema-companion.matchers.kubernetes").setup({ version = "master" }),
-      },
-      schemas = {
-        {
-          name = "Kubernetes Main",
-          uri = require('kubernetes').yamlls_schema()
-        },
-        {
-          name = "Kubernetes v1.30",
-          uri =
-          "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.30.3-standalone-strict/all.json",
-        },
-        {
-          name = "Protolint",
-          uri = "~/.config/nvim/schemas/protolint.json",
-        },
-      },
-    })
+    require("configs.schema-companion")
   end,
 }
 
