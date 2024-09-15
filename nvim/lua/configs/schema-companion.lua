5aea322b75597884c0664c6409d76bbe1a188512
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-09-15 23:30:23 +0000

    [nvim]: disable debug for schema-companion

--- nvim/lua/configs/schema-companion.lua
+++ nvim/lua/configs/schema-companion.lua
@@ -1,26 +1,26 @@
 require("schema-companion").setup({
   -- log_level = vim.log.levels.DEBUG,
-  log_level = vim.log.levels.DEBUG,
+  log_level = vim.log.levels.INFO,
   -- if you have telescope you can register the extension
   enable_telescope = true,
   formatting = true,
-      matchers = {
-        -- add your matchers
-        -- require("schema-companion.matchers.kubernetes").setup({ version = "master" }),
-      },
-      schemas = {
-        {
-          name = "Kubernetes Main",
-          uri = require('kubernetes').yamlls_schema()
-        },
-        {
-          name = "Kubernetes v1.30",
-          uri =
-          "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.30.3-standalone-strict/all.json",
-        },
-        {
-          name = "Protolint",
-          uri = "~/.config/nvim/schemas/protolint.json",
-        },
-      },
+  matchers = {
+    -- add your matchers
+    -- require("schema-companion.matchers.kubernetes").setup({ version = "master" }),
+  },
+  schemas = {
+    {
+      name = "Kubernetes Main",
+      uri = require('kubernetes').yamlls_schema()
+    },
+    {
+      name = "Kubernetes v1.30",
+      uri =
+      "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.30.3-standalone-strict/all.json",
+    },
+    {
+      name = "Protolint",
+      uri = "~/.config/nvim/schemas/protolint.json",
+    },
+  },
 })
