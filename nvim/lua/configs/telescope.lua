c3e42fe2439f028376b3f6225656777839bd77a6
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-09-24 23:47:01 +0100

    [nvim]: config telescope to jump tab for buffer

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -3,3 +3,21 @@ local telescope = require('telescope')
 telescope.load_extension('fzf')
 telescope.load_extension('luasnip')
 telescope.load_extension('notify')
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      -- https://www.reddit.com/r/neovim/comments/xpaqwy/telescope_search_for_files_jump_to_the_tab_if_the/
+      i = {
+        ["<cr>"] = function(bufnr)
+          require("telescope.actions.set").edit(bufnr, "tab drop")
+        end
+      },
+      n = {
+        ["<cr>"] = function(bufnr)
+          require("telescope.actions.set").edit(bufnr, "tab drop")
+        end
+      }
+    }
+  }
+}
