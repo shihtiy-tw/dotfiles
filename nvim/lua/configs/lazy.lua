0eb98603d36c46d757345e37a5e7819c3474107a
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-10-09 17:12:11 +0100

    [nvim]: refactor the plugin import

--- nvim/lua/configs/lazy.lua
+++ nvim/lua/configs/lazy.lua
@@ -25,12 +25,124 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    -- { import = "plugins.render-markdown" },
-    -- { import = "plugins.nvim-treesitter" },
-    -- { import = "plugins.obsidian" },
-    -- { import = "plugins.which-key" },
-    { import = "plugins" },
+    -- { import = "plugins" },
+
+    -- General
+    -- ----------------------------
+    -- enhanced search
+    { import = "plugins.telescope" },
+    -- code outline window
+    { import = "plugins.aerial" },
+    -- autopair plugin that supports multiple characters.
+    { import = "plugins.autopairs" },
+    -- auto save
+    { import = "plugins.autosave" },
+    -- auto create bullets
+    { import = "plugins.bullets" },
+    -- input method select
+    { import = "plugins.im-select" },
+    -- show indent
+    { import = "plugins.indent-blankline" },
+    -- bottom line
+    { import = "plugins.lualine" },
+    -- Library of 40+ independent Lua modules
+    { import = "plugins.mini" },
+    -- a clipboard manager
+    { import = "plugins.neoclip" },
+    -- enhanced notification
+    { import = "plugins.notify" },
+    -- enhanced neovim fold feature
+    { import = "plugins.nvim-ufo" },
+    -- syntax highlighting for rainbow parentheses
+    { import = "plugins.rainbow-delimiters" },
+    -- seartch TODO, BUG etc
+    { import = "plugins.todo-comments" },
+    -- show key mappings
+    { import = "plugins.which-key" },
+
+    -- Notes
+    -- ----------------------------
+    -- Notion
+    { import = "plugins.notion" },
+    -- Obsidian
+    { import = "plugins.obsidian" },
+    -- Obsidian sync scrolling
+    { import = "plugins.obsidian-bridge" },
+
+    -- Search
+    -- ----------------------------
+    { import = "plugins.fzf" },
+
+    -- Navegation
+    -- ----------------------------
+    -- add things to list
+    { import = "plugins.harpoon2" },
+    -- easymotion on lua
+    { import = "plugins.hop" },
+    -- Quickly jump between next and previous NeoVim buffer, tab, file, quickfix, diagnostic
+    { import = "plugins.nap" },
+    -- navegate with vim key
+    { import = "plugins.oil" },
+    -- Open alternative files for the current buffer
+    { import = "plugins.other" },
+    -- tree
+    { import = "plugins.tree" },
+    -- open a url
+    { import = "plugins.url-open" },
+
+    -- Theme
+    -- ----------------------------
+    { import = "plugins.gruvbox" },
+    { import = "plugins.solarized" },
+
+    -- Git
+    -- ----------------------------
+    -- fugitive, diffview
+    { import = "plugins.git" },
+
+    -- Syntax
+    -- ----------------------------
+    { import = "plugins.nvim-treesitter" },
+    -- mason-lsp
+    { import = "plugins.lsp" },
+    -- improve lsp experences in neovim
+    { import = "plugins.lspsaga" },
+    -- mason-conform
+    { import = "plugins.formatter" },
+    -- rename for LSP
+    { import = "plugins.inc-rename" },
+    -- java
+    { import = "plugins.java" },
+    -- selecting schema for yaml and json
+    { import = "plugins.schema-companion" },
+    { import = "plugins.schemastore" },
+    -- snippets
+    { import = "plugins.snippets" },
+    -- trouble
+    { import = "plugins.trouble" },
+
+    -- Kubernetes
+    -- ----------------------------
+    -- use k9s in neovim
+    { import = "plugins.kube-utils" },
+    -- fetches resource definitions from your cluster and feeds them to yamlls
+    { import = "plugins.kubernetes" },
+    -- use kustomize
+    { import = "plugins.kustomize" },
+
+    -- Markdown
+    -- ----------------------------
+    { import = "plugins.markdown" },
+    -- render
+    { import = "plugins.render-markdown" },
+    -- tables
+    { import = "plugins.easy-tables" },
+    { import = "plugins.markdown-table" },
+    -- image
+    { import = "plugins.image" },
   },
+
+
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
