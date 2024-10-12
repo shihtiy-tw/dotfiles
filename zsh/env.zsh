7381a4c5401bf02fd14322aaa6c671d99452bcbd
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-10-12 20:06:25 +0000

    [zsh]: add pyenv ENV

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -30,6 +30,10 @@ export PYTHON3PATH=$(which python3)
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
 #eval "$(pyenv virtualenv-init -)"
 
 # convenience
