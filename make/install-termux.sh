f214f27c245e1275a335102015829c1f7853c654
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-10-17 22:32:51 +0000

    [termux]: add termux install script

---
+++ make/install-termux.sh
@@ -0,0 +1,21 @@
+pkg install git -y
+
+# dotfile
+git clone https://github.com/shihtiy-tw/dotfiles.git
+
+# Go to home dir -
+cd "$HOME"
+
+# Termux Style
+# https://github.com/adi1090x/termux-style
+# Clone this repository (use `gh repo clone adi1090x/termux-style` if you want to use the GitHub CLI)-
+git clone https://github.com/adi1090x/termux-style
+
+# Change to termux-style dir -
+cd termux-style
+
+# To install it, run -
+./install
+
+# And follow the steps, it'll be installed on your system.
+# $ termux-style
