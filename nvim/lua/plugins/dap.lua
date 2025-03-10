312da7deb6d43372d3447ac7b642cb95dbb03ed2
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2025-03-10 08:05:28 +0000

    chore(nvim): get python dap works
    reference:
    - https://github.com/meteora211/AstroNvim/blob/1cef0819cda9a93e485fbb0e10d023402c4ebc45/lua/plugins/mason.lua#L74
    - https://github.com/pylbrecht/dotfiles/blob/549d98ecdffb073d8e3493fed86056439373ebc7/nvim/.config/nvim/lua/debuggers.lua#L2

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,18 +1,46 @@
 return {
-  "williamboman/mason.nvim",
-  "mfussenegger/nvim-dap",
-  "jay-babu/mason-nvim-dap.nvim",
-  "nvim-telescope/telescope-dap.nvim",
-  'theHamsta/nvim-dap-virtual-text',
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "python" })
+      if not opts.handlers then opts.handlers = {} end
+      opts.handlers.python = function() end -- make sure python doesn't get set up by mason-nvim-dap, it's being set up by nvim-dap-python
+    end,
+  },
 
-  "rcarriga/nvim-dap-ui",
-  dependencies = { "nvim-neotest/nvim-nio" },
-
-  'mfussenegger/nvim-dap-python',
-
-
-  config = function()
-    require('configs.dap')
-  end
-
+  {
+    "mfussenegger/nvim-dap-python",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio", 'theHamsta/nvim-dap-virtual-text', "nvim-telescope/telescope-dap.nvim" },
+    ft = "python", -- NOTE: ft: lazy-load on filetype
+    config = function(_, opts)
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      if vim.fn.has "win32" == 1 then
+        path = path .. "/venv/Scripts/python"
+      else
+        path = path .. "/venv/bin/python"
+      end
+      require("dap-python").setup(path, opts)
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio", 'theHamsta/nvim-dap-virtual-text', "nvim-telescope/telescope-dap.nvim" },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end
+  }
 }
