70da9057817a95b4a250a9313b12637875a7cd09
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2025-06-28 20:50:51 +0100

    chore(nvim): add daily note for obsidian

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -59,6 +59,30 @@ return {
     },
     disable_frontmatter = false,
 
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "Calendar/Planner/05Daily/",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d-%a",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "Planner/Template_Planner_Daily_Today.md",
+      -- Optional, if you want `Obsidian yesterday` to return the last work day or `Obsidian tomorrow` to return the next work day.
+      workdays_only = false,
+    },
+    templates = {
+      folder = "Atlas/Utilities/Templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function.
+      -- Functions are called with obsidian.TemplateContext objects as their sole parameter.
+      -- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#substitutions
+      substitutions = {},
+    },
+
     completion = {
       -- Set to false to disable completion.
       nvim_cmp = true,
