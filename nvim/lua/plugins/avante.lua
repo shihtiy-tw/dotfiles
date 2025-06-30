59520ad5f5f30596db14caa0ba8de1eb67d87eea
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2025-06-30 12:44:45 +0100

    chore(nvim): add avante config

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -2,44 +2,10 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
+  config = function()
+    require("configs.avante")
+  end,
   opts = {
-    -- add any opts here
-    -- for example
-    -- https://github.com/yetone/avante.nvim/issues/2048j
-    mode = "legacy", -- solve cannnot apply change issue for local LLM
-    -- mode = "agentic",
-    provider = "ollama",
-    auto_suggestions_provider = "ollama",
-    providers = {
-      -- bedrock = {
-      --   model = "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
-      --   aws_profile = "default",
-      --   aws_region = "us-east-1",
-      -- },
-      ollama = {
-        __inherited_from = "openai",
-        endpoint = "http://localhost:11434/v1",
-        api_key_name = "",
-        --model = "deepseek-r1:1.5b",
-        model = "qwen2.5-coder:3b",
-        -- model = "deepseek-coder-v2:16b",
-        -- important to set this to true if you are using a local server
-        disable_tools = true,
-        max_tokens = 32768,
-      },
-    },
-    windows = {
-      edit = { border = "rounded" },
-      ask = {
-        floating = false,    -- Open the 'AvanteAsk' prompt in a floating window
-        start_insert = true, -- Start insert mode when opening the ask window
-        border = "rounded",
-      },
-    },
-    cursor_applying_provider = "ollama",
-    behaviour = {
-      enable_cursor_planning_mode = true,
-    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
